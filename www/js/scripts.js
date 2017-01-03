6133923bffd68cdad36dc188e2b2ce6032efb282
Trabajocreativo.com <paisleones@gmail.com>
2017-01-03 17:45:56 +0100

    17:45

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -106,6 +106,7 @@ function validar_nuevo_rincon()
     var titulo_rincon = $('#titulo').val();
     var descripcion_rincon = $('#descripcion').val();
     var rating_rincon = $("#estrellas").html();
+    var localizacion_rincon = $("#localizacion").val();
     var categoria_rincon = $("#tipo_de_categoria_nuevo").html();
     var tipo_rincon = $("#tipo_de_rincon_nuevo").html();
     var uri_video_rincon = $('#uri_video').html();
