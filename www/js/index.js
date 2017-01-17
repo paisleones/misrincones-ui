130d1139d643ed6f57671282bb1e87c4b062adaf
Trabajocreativo.com <paisleones@gmail.com>
2017-01-17 20:06:09 +0100

    20:06

---
+++ www/js/index.js
@@ -0,0 +1,468 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    console.log("navigator.geolocation works well");
+
+
+    // Avisar de que se perdió la conexión.
+    document.addEventListener("offline", function () {
+        $("#error").show();
+        navigator.notification.alert("No tienes conexión a internet. Ten en cuenta que las secciones pueden verse afectadas", null, "Sin conexión", "Aceptar");
+    });
+
+}
+
+var app = {
+    // Application Constructor
+    initialize: function () {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function () {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    }
+
+};
+
+
+function ShowExitDialog() {
+    navigator.notification.confirm(
+            ("Quieres salir de la aplicacion?"), // message
+            alertexit, // callback
+            'Mensaje de Misrincones', // title
+            'ACEPTAR,CANCELAR' // buttonName
+            );
+
+}
+
+function alertexit(button) {
+
+    if (button == "1" || button == 1)
+    {
+
+        navigator.app.exitApp();
+    }
+
+}
+
+
+document.addEventListener("backbutton", ShowExitDialog, false);
+
+
+
+var source;
+var pictureSource;   // picture source
+var destinationType; // sets the format of returned value
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+    pictureSource = navigator.camera.PictureSourceType;
+    destinationType = navigator.camera.DestinationType;
+}
+
+function libreria_fotos(source) {
+    // Retrieve image file location from specified source
+    navigator.camera.getPicture(onPhotoURISuccess, onFail, {quality: 50,
+        destinationType: destinationType.FILE_URI,
+        sourceType: source});
+}
+
+
+function libreria_videos() {
+    // Retrieve image file location from specified source
+    navigator.camera.getPicture(libreria_videos_Success, onFail, {quality: 50,
+        mediaType: window.Camera.MediaType.VIDEO,
+        destinationType: window.Camera.DestinationType.FILE_URI,
+        sourceType: Camera.PictureSourceType.PHOTOLIBRARY});
+}
+
+function libreria_videos_Success(videoURI) {
+    var video = document.getElementById('videoLocal');
+    video.src = videoURI.fullPath;
+    document.getElementById("uri_video").innerHTML = videoURI;
+    //subirImagen(imageURI)
+}
+
+function onPhotoURISuccess(imageURI) {
+    var largeImage = document.getElementById('fotoLocal');
+
+    largeImage.style.display = 'block';
+
+    largeImage.src = imageURI;
+    document.getElementById("uri_foto").innerHTML = largeImage.src;
+}
+
+
+function getPhoto(source) {
+    // Retrieve image file location from specified source
+    navigator.camera.getPicture(onSuccess, onFail, {quality: 50,
+        destinationType: destinationType.FILE_URI,
+        sourceType: source});
+}
+
+function hacerFoto(source) {
+    //navigator.camera.getPicture(onSuccess, onFail, {quality: 50, destinationType: Camera.DestinationType.FILE_URI, sourceType: source});
+
+    navigator.camera.getPicture(onSuccess, onFail, {quality: 75,
+        destinationType: Camera.DestinationType.FILE_URI,
+        sourceType: source,
+        allowEdit: false,
+        encodingType: Camera.EncodingType.JPEG,
+        targetWidth: 500,
+        targetHeight: 500,
+        popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: false});
+}
+
+function onSuccess(imageURI) {
+    var image = document.getElementById('fotoLocal');
+    image.src = imageURI;
+    document.getElementById("uri_foto").innerHTML = image.src;
+    //subirImagen(imageURI)
+}
+
+function grabarvideo()
+{
+    navigator.device.capture.captureVideo(captureVideoSuccess, onFail, {
+        destinationType: Camera.DestinationType.FILE_URI,
+        targetWidth: 500,
+        targetHeight: 500,
+        duration: 30,
+        limit: 1,
+        quality: 0
+    });
+}
+
+function captureVideoSuccess(videoURI) {
+    var video = document.getElementById('videoLocal');
+    video.src = videoURI[0].fullPath;
+    document.getElementById("uri_video").innerHTML = video.src;
+    //subirImagen(imageURI)
+}
+
+
+
+function onFail(message) {
+
+}
+
+function generar(longitud)
+{
+    long = parseInt(longitud);
+    var caracteres = "abcdefghijkmnpqrtuvwxyzABCDEFGHIJKLMNPQRTUVWXYZ2346789";
+    var contrasena = "";
+    for (i = 0; i < long; i++)
+        contrasena += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+    return contrasena;
+}
+
+function getReverseGeocodingData(lat, lng) {
+    var latlng = new google.maps.LatLng(lat, lng);
+    // This is making the Geocode request
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'latLng': latlng}, function (results, status) {
+        if (status !== google.maps.GeocoderStatus.OK) {
+            alert(status);
+        }
+        // This is checking to see if the Geoeode Status is OK before proceeding
+        if (status == google.maps.GeocoderStatus.OK) {
+            console.log(results);
+            var address = (results[0].formatted_address);
+            document.getElementById("localizacion").value = address;
+            document.getElementById("direccion_rincon").innerHTML = address;
+        }
+    });
+}
+
+function subirImagen(id) {
+    var identificador = id;
+    var fileURL = document.getElementById("uri_foto").innerHTML;
+    var options = new FileUploadOptions();
+    options.fileKey = "imagen";
+    options.fileName = fileURL.substr(fileURL.lastIndexOf('/') + 1);
+
+    var ft = new FileTransfer();
+    ft.upload(fileURL, encodeURI("http://misrincones.trabajocreativo.com/app/upload_foto.php?id=" + identificador), uploadSuccess, uploadFail, options);
+}
+
+function subirVideo(id) {
+    var identificador = id;
+    var fileURL = document.getElementById("uri_video").innerHTML;
+    var options = new FileUploadOptions();
+    options.fileKey = "video";
+    options.fileName = fileURL.substr(fileURL.lastIndexOf('/') + 1);
+    var ft = new FileTransfer();
+    ft.upload(fileURL, encodeURI("http://misrincones.trabajocreativo.com/app/upload_video.php?id=" + identificador), uploadSuccess, uploadFail, options);
+}
+
+function uploadSuccess(r) {
+    //alert("Code = " + r.responseCode + " Response = " + r.response + " Sent = " + r.bytesSent);
+
+}
+
+function uploadFail(error) {
+    navigator.notification.alert("Los sentimos, pero se ha producido un error en la carga de datos.", null, "Mensaje de misrincones", "Aceptar");
+}
+
+
+function guardar_rincon()
+{
+    subirImagen(id);
+    subirVideo(id);
+
+    $.ajax({
+        type: 'post',
+        url: 'http://misrincones.trabajocreativo.com/app/actualizar_rincon.php?id=' + id,
+        data: $('#main').serialize(),
+        success: function () {
+            navigator.notification.alert("Se estan actualizando los datos. En 24/48 horas aproximadamente tu rincon estará visible. Muchas gracias.", null, "Mensaje de misrincones", "Aceptar");
+            load_url('nuevo_rincon', 'rincon_ok.html');
+            setTimeout(load_url('polivalente', 'http://misrincones.trabajocreativo.com/app/enviar_email.php?id=' + id), 1000);
+
+
+        }
+    });
+
+
+}
+
+function guardar_rincon_video()
+{
+    var id = generar(20);
+    subirVideo(id);
+}
+
+var styles = [
+    {
+        featureType: 'water', // set the water color
+        elementType: 'geometry.fill', // apply the color only to the fill
+        stylers: [
+            {color: '#96cbcc'}
+        ]
+    }, {
+        featureType: 'landscape.natural', // set the natural landscape
+        elementType: 'all',
+        stylers: [
+            {color: '#ecf0f1'},
+            {lightness: 0}
+        ]
+    }
+    , {
+        featureType: 'poi', // set the point of interest
+        elementType: 'geometry.stroke',
+        stylers: [
+            {color: '#ffffff'},
+            {lightness: 0}
+        ]
+    },
+    {//poi stands for point of interest - don't show these lables on the map
+        featureType: "poi",
+        elementType: "labels",
+        stylers: [
+            {visibility: "off"}
+        ]
+    },
+    {
+        featureType: "poi.business",
+        elementType: "labels",
+        stylers: [
+            {visibility: "off"}
+        ]},
+    {
+        featureType: 'road', // set the road
+        elementType: 'geometry',
+        stylers: [
+            {color: '#ffffff'},
+            {lightness: 0}
+        ]
+    }, {
+        featureType: 'road.highway', // set the local road
+        elementType: 'labels',
+        stylers: [
+            {visibility: "off"}
+        ]
+    }
+];
+
+
+
+function cargar_mapa(latitud, longitud) {
+    var map;
+    function initialize() {
+
+        var _latitude = latitud;
+        var _longitude = longitud;
+
+
+        var myLatlng = new google.maps.LatLng(_latitude, _longitude);
+
+        google.maps.event.addDomListener(zoomout, 'click', function () {
+            var currentZoomLevel = map.getZoom();
+            if (currentZoomLevel != 0) {
+                map.setZoom(currentZoomLevel - 1);
+            }
+
+        });
+
+        google.maps.event.addDomListener(zoomin, 'click', function () {
+            var currentZoomLevel = map.getZoom();
+            if (currentZoomLevel != 21) {
+                map.setZoom(currentZoomLevel + 1);
+            }
+
+        });
+
+        var myOptions = {
+            disableDefaultUI: true,
+            scrollwheel: false,
+            styles: styles,
+            zoom: 15,
+            center: myLatlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+        var marker = new google.maps.Marker({
+            icon: "img/marcador.png",
+            size: new google.maps.Size(44, 50),
+            //animation: google.maps.Animation.DROP,
+            draggable: true,
+            position: myLatlng,
+            map: map,
+            title: "Estas aqui"
+        });
+
+        google.maps.event.addListener(marker, 'dragend', function (event) {
+            getReverseGeocodingData(event.latLng.lat(), event.latLng.lng());
+            document.getElementById("lat").value = event.latLng.lat();
+            document.getElementById("long").value = event.latLng.lng();
+        });
+
+
+        google.maps.event.addListener(marker, 'dragend', function (event) {
+            posset = 1;
+
+            if (map.getZoom() < 10) {
+                map.setZoom(10);
+            }
+            map.setCenter(event.latLng);
+            computepos(event.latLng);
+            drag = true;
+            setTimeout(function () {
+                drag = false;
+            }, 250);
+        });
+
+    }
+    google.maps.event.addDomListener(window, "load", initialize());
+}
+
+
+function obtener() {
+    navigator.geolocation.getCurrentPosition(mostrar);
+}
+
+function mostrar(posicion) {
+    var _latitude = posicion.coords.latitude;
+    var _longitude = posicion.coords.longitude;
+
+    document.getElementById("lat").value = _latitude;
+    document.getElementById("long").value = _longitude;
+
+    getReverseGeocodingData(_latitude, _longitude);
+
+    cargar_mapa(_latitude, _longitude);
+}
+
+
+function play_video(videoUrl)
+{
+    // Play a video with callbacks
+    var options = {
+        successCallback: function () {
+            console.log("Video was closed without error.");
+        },
+        errorCallback: function (errMsg) {
+            console.log("Error! " + errMsg);
+        },
+        orientation: 'landscape'
+    };
+    window.plugins.streamingMedia.playVideo(videoUrl, options);
+}
+
+
+function fotoDialog() {
+    navigator.notification.confirm(
+            (""), // message
+            alertfoto, // callback
+            'Añadir una foto a este lugar', // title
+            'CÁMARA  ,  GALERÍA' // buttonName
+            );
+
+}
+
+function alertfoto(button) {
+
+    if (button == "1" || button == 1)
+    {
+
+        hacerFoto();
+    }
+
+    if (button == "2" || button == 2)
+    {
+
+        getPhoto(pictureSource.SAVEDPHOTOALBUM);
+    }
+
+}
+
+
+function videoDialog() {
+    navigator.notification.confirm(
+            (""), // message
+            alertvideo, // callback
+            'Añadir un video a este lugar', // title
+            'GRABAR  ,  GALERÍA' // buttonName
+            );
+
+}
+
+function alertvideo(button) {
+
+    if (button == "1" || button == 1)
+    {
+
+        grabarvideo();
+    }
+
+    if (button == "2" || button == 2)
+    {
+
+        libreria_videos();
+    }
+
+}
+
---
+++ www/js/scripts.js
@@ -0,0 +1,268 @@
+function SimularClick(idObjeto) {
+
+    var nouEvent = document.createEvent("MouseEvents");
+    nouEvent.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    var objecto = document.getElementById(idObjeto);
+    var canceled = !objecto.dispatchEvent(nouEvent);
+}
+
+function load_url(div, url)
+{
+    $.ajax({
+        url: url,
+        cache: true,
+        type: 'GET',
+        contentType: "html",
+        async: true,
+        crossDomain: true,
+        success: function (data) {
+            $('#' + div).html(data);
+            $("#" + div).show();
+        }
+    });
+}
+
+function load_url_ajax(div, url)
+{
+    $.ajax(
+            {
+                url: url,
+                cache: true,
+                type: 'GET',
+                contentType: "html",
+                async: true,
+                crossDomain: true,
+            })
+            .fail(function (data) {
+                alert("Lo sentimos, pero se ha producido un error en la carga de datos");
+            })
+            .done(function () {
+                $('#loading').show();
+            })
+            .always(function (data) {
+                $("#" + div).html(data);
+                $("#" + div).show();
+                $('#loading').hide();
+            });
+}
+;
+function activar_tipo_mapa(tipo)
+{
+    document.getElementById('tipo_de_rincon').innerHTML = tipo;
+}
+
+function validar_registro()
+{
+    $("#validar_registro").hide();
+    $("#comprobando_registro").show();
+    var email_registro = $('#email_registro').val();
+    var clave_registro = $('#clave_registro').val();
+    var usuario_registro = $('#usuario_registro').val();
+    var aceptar_condiciones = $('#aceptar_condiciones').val();
+
+
+    if (email_registro == "")
+    {
+        $("#email_registro").addClass("input_login_error");
+        var validar1 = 0;
+    } else
+    {
+        $("#email_registro").removeClass("input_login_error");
+        var validar1 = 1;
+    }
+
+    if (clave_registro == "")
+    {
+        $("#clave_registro").addClass("input_login_error");
+        var validar2 = 0;
+    } else
+    {
+        $("#clave_registro").removeClass("input_login_error");
+        var validar2 = 1;
+    }
+
+    if (usuario_registro == "")
+    {
+        $("#usuario_registro").addClass("input_login_error");
+        var validar3 = 0;
+    } else
+    {
+        $("#usuario_registro").removeClass("input_login_error");
+        var validar3 = 1;
+    }
+
+    if (validar1 == 1 && validar2 == 1 && validar3 == 1)
+    {
+        load_url("polivalente", "http://misrincones.trabajocreativo.com/app/registro_usuario.php?usuario_registro=" + usuario_registro + "&email_registro=" + email_registro + "&clave_registro=" + clave_registro, "crear_nueva_cuenta");
+    } else
+    {
+        $("#comprobando_registro").hide();
+        $("#validar_registro").show();
+        navigator.notification.alert("Un momento!, debes rellenar todos los campos", null, "Mensaje de misrincones", "Aceptar");
+    }
+}
+
+
+function iniciar_sesion()
+{
+
+    $("#validar_login").hide();
+    $("#comprobando_login").show();
+    var email_sesion = $('#email_sesion').val();
+    var clave_sesion = $('#clave_sesion').val();
+
+    if (email_sesion == "")
+    {
+        $("#email_sesion").addClass("input_login_error");
+        var validar5 = 0;
+    } else
+    {
+        $("#email_sesion").removeClass("input_login_error");
+        var validar5 = 1;
+    }
+
+    if (clave_sesion == "")
+    {
+        $("#clave_sesion").addClass("input_login_error");
+        var validar6 = 0;
+    } else
+    {
+        $("#clave_sesion").removeClass("input_login_error");
+        var validar6 = 1;
+    }
+
+    if (validar5 == 1 && validar6 == 1)
+    {
+        /*
+         $.ajax({
+         type: "POST",
+         data: $("#login_form").serialize(),
+         dataType: 'html',
+         url: "http://misrincones.trabajocreativo.com/app/login1.php",
+         success: function (data) {
+         alert('EXITO');
+         },
+         error: function () {
+         alert('ERROR');
+         }
+         });
+         */
+        load_url("polivalente", "http://misrincones.trabajocreativo.com/app/login.php?email_sesion=" + email_sesion + "&clave_sesion=" + clave_sesion, "crear_nueva_cuenta");
+    } else
+    {
+        $("#comprobando_login").hide();
+        $("#validar_login").show();
+        navigator.notification.alert("Un momento!, debes rellenar todos los campos", null, "Mensaje de misrincones", "Aceptar");
+    }
+
+}
+
+
+function validar_nuevo_rincon()
+{
+    $("#boton_nuevo_rincon").hide();
+    $("#comprobando_rincon").show();
+    var titulo_rincon = $('#titulo').val();
+    var descripcion_rincon = $('#descripcion').val();
+    var rating_rincon = $("#estrellas").val();
+    var localizacion_rincon = $("#localizacion").val();
+    var categoria_rincon = $("#tipo_de_categoria_nuevo").html();
+    var tipo_rincon = $("#tipo_de_rincon_nuevo").html();
+    var uri_video_rincon = $('#uri_video').html();
+
+    if (categoria_rincon == "Categoría de rincón?")
+    {
+        $("#boton_categoria").addClass("input_login_error");
+        var validar1 = 0;
+
+    } else
+    {
+        $("#boton_categoria").removeClass("input_login_error");
+        var validar1 = 1;
+    }
+
+    if (tipo_rincon == "Tipo de rincón?")
+    {
+        $("#boton_tipo").addClass("input_login_error");
+        var validar2 = 0;
+
+    } else
+    {
+        $("#boton_tipo").removeClass("input_login_error");
+        var validar2 = 1;
+    }
+
+    if (titulo_rincon == "")
+    {
+        $("#titulo").addClass("input_login_error");
+        var validar3 = 0;
+    } else
+    {
+        $("#titulo").removeClass("input_login_error");
+        var validar3 = 1;
+    }
+
+    if (descripcion_rincon == "")
+    {
+        $("#descripcion").addClass("input_login_error");
+        var validar4 = 0;
+    } else
+    {
+        $("#descripcion").removeClass("input_login_error");
+        var validar4 = 1;
+    }
+
+    if (rating_rincon == "")
+    {
+        $("#rating_rincon").addClass("input_login_error");
+        var validar5 = 0;
+    } else
+    {
+        $("#rating_rincon").removeClass("input_login_error");
+        var validar5 = 1;
+    }
+
+    if (uri_video_rincon == "")
+    {
+        $("#rincon_video").addClass("input_login_error");
+        var validar6 = 0;
+    } else
+    {
+        $("#rincon_video").removeClass("input_login_error");
+        var validar6 = 1;
+    }
+
+    if (localizacion_rincon == "" || localizacion_rincon != "Obteniendo localización ...")
+    {
+        var validar7 = 1;
+    }
+
+    if (validar1 == 1 && validar2 == 1 && validar3 == 1 && validar4 == 1 && validar5 == 1 && validar6 == 1 && validar7 == 1)
+    {
+        guardar_rincon();
+    } else
+    {
+        $("#comprobando_rincon").hide();
+        $("#boton_nuevo_rincon").show();
+        navigator.notification.alert("Un momento!, debes rellenar todos los campos", null, "Mensaje de misrincones", "Aceptar");
+    }
+
+}
+
+
+function reproducir_video(id_video)
+{
+    document.getElementById("video3dark").innerHTML = "<source src='http://misrincones.trabajocreativo.com/" + id_video + "' type='video/mp4' />";
+    SimularClick("play_video");
+}
+
+
+function SimularClick(idObjeto) {
+
+    var nouEvent = document.createEvent("MouseEvents");
+    nouEvent.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+
+    var objecto = document.getElementById(idObjeto);
+    var canceled = !objecto.dispatchEvent(nouEvent);
+}
+
