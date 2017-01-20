91b607647143370c1cdc24559917da6de9403df3
Trabajocreativo.com <paisleones@gmail.com>
2017-01-20 07:30:21 +0100

    7:30

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -105,19 +105,11 @@ function validar_registro()
         }
 
         if ($("#aceptar_condiciones").is(':checked')) {
+
             var id = generar(20);
-            $.ajax({
-                type: 'post',
-                url: 'http://misrincones.trabajocreativo.com/app/registro_usuario.php?id=' + id,
-                data: $('#main').serialize(),
-                success: function () {
-                    navigator.notification.alert("Enhorabuena, se ha completado el registro. Revise su correo electronico para confirmar su cuenta de usuario. Muchas gracias.", null, "Mensaje de misrincones", "Aceptar");
-                    load_url('nuevo_rincon', 'registro_ok.html');
-                    setTimeout(load_url('polivalente', 'http://misrincones.trabajocreativo.com/app/enviar_email_registro.php?id=' + id), 1000);
+            load_url("polivalente", "http://misrincones.trabajocreativo.com/app/registro_usuario.php?id=" + id + "&usuario_registro=" + usuario_registro + "&email_registro=" + email_registro + "&clave_registro=" + clave_registro)
 
 
-                }
-            });
 
         } else {
             $("#comprobando_registro").hide();
@@ -297,4 +289,3 @@ function SimularClick(idObjeto) {
     var objecto = document.getElementById(idObjeto);
     var canceled = !objecto.dispatchEvent(nouEvent);
 }
-
