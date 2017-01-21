d586b24853541d51566a9d8c683351f82c798aea
Trabajocreativo.com <paisleones@gmail.com>
2017-01-21 12:40:07 +0100

    12:39

--- www/js/index.js
+++ www/js/index.js
@@ -20,15 +20,14 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
     console.log("navigator.geolocation works well");
-
-
-    // Avisar de que se perdió la conexión.
-    document.addEventListener("offline", function () {
-        $("#error").show();
-        navigator.notification.alert("No tienes conexión a internet. Ten en cuenta que las secciones pueden verse afectadas", null, "Sin conexión", "Aceptar");
-    });
-
 }
+
+// Avisar de que se perdió la conexión.
+document.addEventListener("offline", function () {
+    $("#error").show();
+    navigator.notification.alert("No tienes conexión a internet. Ten en cuenta que las secciones pueden verse afectadas", null, "Sin conexión", "Aceptar");
+});
+
 
 var app = {
     // Application Constructor
