22db46b0ddc872e45783a3720654c4cc9709ccb4
Trabajocreativo.com <paisleones@gmail.com>
2017-03-12 00:40:59 +0100

    0:40

--- www/js/index.js
+++ www/js/index.js
@@ -183,6 +183,13 @@ var styles = [
         ]},
     {
         featureType: "transit.station",
+        elementType: "labels",
+        stylers: [
+            {visibility: "off"}
+        ]},
+    {
+        featureType: "transit.station.bus",
+        elementType: "labels",
         stylers: [
             {visibility: "off"}
         ]},
