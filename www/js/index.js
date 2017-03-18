eaaec74328fdfeacb39d00631c90d4e45c192aad
Trabajocreativo.com <paisleones@gmail.com>
2017-03-18 10:59:32 +0100

    10:59

--- www/js/index.js
+++ www/js/index.js
@@ -95,6 +95,23 @@ function getReverseGeocodingData(lat, lng) {
     });
 }
 
+function getReverseGeocodingData_mapa_principal(lat, lng) {
+    var latlng = new google.maps.LatLng(lat, lng);
+    // This is making the Geocode request
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'latLng': latlng}, function (results, status) {
+        if (status !== google.maps.GeocoderStatus.OK) {
+            alert(status);
+        }
+        // This is checking to see if the Geoeode Status is OK before proceeding
+        if (status == google.maps.GeocoderStatus.OK) {
+            console.log(results);
+            var address = (results[0].formatted_address);
+            document.getElementById("direccion_mapa").innerHTML = address;
+        }
+    });
+}
+
 // Funcion para haccer upload de un video al servidor
 
 function subirVideo(id) {
