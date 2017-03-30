0c3ebb4b322b877aba8fc328d33427f740800923
Trabajocreativo.com <paisleones@gmail.com>
2017-03-30 21:25:33 +0200

    21:25

--- www/js/mapa/maps_mobile.js
+++ www/js/mapa/maps_mobile.js
@@ -63,10 +63,10 @@
 if ($(window).width() > alto_pantalla) {
 
 $('.map-canvas').height($(window).height() - $('.header').height() + 70);
-}
+        }
 else {
 $('.map-canvas #map').height($(window).height() - $('.header').height() + 70);
-}
+        }
 }
 
 $('.mapa_nuevo_rincon').height($(window).height() - $('.header').height() - 100);
@@ -446,7 +446,7 @@ var mapCenter = new google.maps.LatLng(json.latitude, json.longitude, zoom);
                 flat: true
         });
         marker.content.className = 'marker-loaded';
-}
+        }
 
 
 
@@ -503,33 +503,23 @@ var _latitude = json.data[a].latitude;
         var _longitude = json.data[a].longitude;
         var mapCenter = new google.maps.LatLng(_latitude, _longitude);
         map.setCenter(mapCenter);
-}
-});
         }
+});
+}
 
 // Create modal if more items are on the same location (example: one building with floors) -----------------------------
 
 function multiChoice(sameLatitude, sameLongitude, json) {
 //if (clickedCluster.getMarkers().length > 1){
 var multipleItems = [];
-        $.each(json.data, function(a) {
-        if (json.data[a].latitude == sameLatitude && json.data[a].longitude == sameLongitude) {
-        pushItemsToArray(json, a, json.data[a].category, multipleItems);
-        }
-        });
-        $('body').append('<div class="modal-window multichoice fade_in"></div>');
-        $('.modal-window').load('https://www.mycorner360.com/assets/external/_modal-multichoice.html', function() {
-$('.modal-window .modal-wrapper .items').html(multipleItems);
-        rating('.modal-window');
-});
-        $('.modal-window .modal-background, .modal-close').live('click', function(e){
-$('.modal-window').addClass('fade_out');
-        setTimeout(function() {
-        $('.modal-window').remove();
-        }, 300);
-});
+        $("#volver_atras").show();
+//$("#loading").show();
+        SimularClick('cerrar_izquierda');
+        load_url("pantalla_mis_rincones", "multichoice.html");
+        $("#pantalla_mis_rincones").show();
+        load_url("multichoice", "https://www.mycorner360.com/app/_modal-multichoice.php?latitud=" + sameLatitude + "&longitud=" + sameLongitude);
         //}
-        }
+}
 
 // Animate OSM marker --------------------------------------------------------------------------------------------------
 
@@ -557,7 +547,7 @@ var bounds = map.getBounds();
 
         $('.items-list .results').html(visibleItemsArray);
         rating('.results .item');
-        }
+}
 
 // Redraw map after item list is closed --------------------------------------------------------------------------------
 
@@ -567,10 +557,10 @@ $('.map-canvas').toggleClass('results-collapsed');
         $('.map-canvas .map').one("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
 if (mapProvider == 'osm'){
 map.invalidateSize();
-}
+        }
 else if (mapProvider == 'google'){
 google.maps.event.trigger(map, 'resize');
-}
-});
-});
         }
+});
+        });
+}
