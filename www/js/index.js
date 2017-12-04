0090eb88d64ab2fc4655a0a80735ba2059c02498
Trabajocreativo.com <paisleones@gmail.com>
2017-12-04 12:38:53 +0100

    12:38

--- www/js/index.js
+++ www/js/index.js
@@ -11,6 +11,7 @@ document.addEventListener("offline", function () {
 
 // Notificaciones push
 document.addEventListener('deviceready', function () {
+    window.analytics.setUserId('UA-92835463-1');
     // Enable to debug issues.
     // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
 
