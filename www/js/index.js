70f859d219f6aedd2fa58c2c46e65f6048eb100e
Trabajocreativo.com <paisleones@gmail.com>
2017-12-04 23:37:23 +0100

    23:37

--- www/js/index.js
+++ www/js/index.js
@@ -2,7 +2,8 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 
     //console.log("navigator.geolocation works well");
-    //window.ga.startTrackerWithId('UA-92835463-1', 30);
+    window.ga.startTrackerWithId('UA-92835463-1', 30);
+    window.ga.trackView('Mycorner360 APP');
 
 }
 
